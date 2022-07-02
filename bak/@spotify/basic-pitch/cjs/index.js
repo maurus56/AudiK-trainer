610a721774134476a28830c6365ac1df7feb77e9
maurus56 <maurus56@gmail.com>
2022-07-02 13:10:41 +0200

    bak of dependency modifications

---
+++ bak/@spotify/basic-pitch/cjs/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.generateFileData = exports.outputToNotesPoly = exports.noteFramesToTime = exports.addPitchBendsToNoteEvents = exports.BasicPitch = void 0;
+var inference_1 = require("./inference");
+Object.defineProperty(exports, "BasicPitch", { enumerable: true, get: function () { return inference_1.BasicPitch; } });
+var toMidi_1 = require("./toMidi");
+Object.defineProperty(exports, "addPitchBendsToNoteEvents", { enumerable: true, get: function () { return toMidi_1.addPitchBendsToNoteEvents; } });
+Object.defineProperty(exports, "noteFramesToTime", { enumerable: true, get: function () { return toMidi_1.noteFramesToTime; } });
+Object.defineProperty(exports, "outputToNotesPoly", { enumerable: true, get: function () { return toMidi_1.outputToNotesPoly; } });
+Object.defineProperty(exports, "generateFileData", { enumerable: true, get: function () { return toMidi_1.generateFileData; } });
+//# sourceMappingURL=index.js.map
---
+++ bak/@spotify/basic-pitch/esm/index.js
@@ -0,0 +1,3 @@
+export { BasicPitch } from './inference';
+export { addPitchBendsToNoteEvents, noteFramesToTime, outputToNotesPoly, generateFileData, } from './toMidi';
+//# sourceMappingURL=index.js.map

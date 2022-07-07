c006082a37f48b9133134993b321f794fc4fe5ec
maurus56 <maurus56@gmail.com>
2022-07-07 19:13:03 +0200

    new pitch detention implementation on component

--- src/app/services/general-note-detector.service.ts
+++ src/app/services/general-note-detector.service.ts
@@ -5,21 +5,16 @@ import { Injectable } from '@angular/core';
 })
 export class GeneralNoteDetectorService {
 
-  note:string = "";
-  frequency:string = "";
-  cents:string = "";
-
+  // Predefine variables
+  private audioSource: MediaStreamAudioSourceNode | undefined;
+  private audioAnalyser: AnalyserNode;
+  private audioStream: MediaStream | undefined;
   private audioContext: AudioContext | undefined;
   private mediaDevices: MediaDevices;
 
   private notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
   private A4 = 440;
   private C0 = Math.round(this.A4 * Math.pow(2, -4.75)); // 16
-
-  // Predefine variables
-  private audioSource: MediaStreamAudioSourceNode | undefined;
-  private audioAnalyser: AnalyserNode;
-  private audioStream: MediaStream | undefined;
 
   private frequencies: Float32Array | undefined;
   private amplitude: Uint8Array | undefined;
@@ -28,10 +23,10 @@ export class GeneralNoteDetectorService {
   private options = {
     microphone: true,
     audioFile: false,
-		callback: this.analyserCallback,
-		returnNote: true,
-		returnCents: true,
-		decimals: 2,
+    callback: this.callback,
+    returnNote: true,
+    returnCents: true,
+    decimals: 2,
   };
 
   private lastFrequency: number | null = null;
@@ -42,8 +37,6 @@ export class GeneralNoteDetectorService {
     // Initialize and assign a audio analyser
     this.audioAnalyser = this.audioContext!.createAnalyser();
   }
-
-
 
   // Code by fritzvd (signaltohertz) - https://github.com/fritzvd/signaltohertz
   // Changes: function name
@@ -83,12 +76,11 @@ export class GeneralNoteDetectorService {
     return octave;
   };
 
-  calculateCents(currentFrequency: number, lastFrequency: number) {
-    const cents = 1200 * Math.log2(lastFrequency / currentFrequency);
-    return cents;
+  calculateCents(currentFrequency: number, lastFrequency: number | null) {
+    return lastFrequency ? 1200 * Math.log2(lastFrequency / currentFrequency) : 0;
   };
 
-  calculateNote(frequency:number) {
+  calculateNote(frequency: number) {
     const semiTone = this.calculateSemiTone(frequency);
     const octave = this.calculateOctave(semiTone);
     const notePosition = Math.floor(semiTone % 12);
@@ -101,22 +93,52 @@ export class GeneralNoteDetectorService {
     throw new Error(`Something went wrong: ${err}`);
   };
 
-  logError(err: string) {
-    console.error(err);
-  };
+  // Get the frequencies and return values based on options
+  analysePitch() {
+    console.log(this.frequencies)
+    this.audioAnalyser.getFloatFrequencyData(this.frequencies!);
 
-  initAnalyser() {
-    return this.initialize();
+    const frequency = this.calculateFrequency(this.frequencies!);
+
+    if (frequency) {
+      const { returnCents, returnNote, decimals, callback } = this.options;
+
+      const returnValue = {
+        frequency: frequency.toFixed(decimals),
+        note: returnNote ? this.calculateNote(frequency) : "",
+        cents: returnCents ? this.calculateCents(frequency, this.lastFrequency).toFixed(decimals) : "",
+      };
+
+      this.lastFrequency = frequency;
+      // Execute the callback. (Intended for returning the output)
+      callback(returnValue);
+
+    }
+
+    // Tells the browser we wish to perform an animation. Call callback before re-paint
+    if (this.audioContext && this.audioContext.state === 'running') {
+      window.requestAnimationFrame(this.analysePitch);
+    }
   }
 
-  startAnalyser() {
-    this.analysePitch();
+  // What to do with the audio payload
+  callback(payload: { frequency: string; note: string; cents: string; }) {
+  }
+
+  initialize(args: any) {
+    // Start media stream
+    this.options = { ...this.options, ...args };
+    return this.mediaDevices.getUserMedia({ audio: true })
+      .then(stream => {
+        this.initiateStream(stream);
+      })
+      .catch(this.throwError);
   }
 
   resumeAnalyser() {
     if (this.audioContext?.state === 'suspended') {
       this.audioContext.resume().then(() => {
-        this.startAnalyser();
+        this.analysePitch();
       });
     }
   }
@@ -127,58 +149,11 @@ export class GeneralNoteDetectorService {
     }
   }
 
-  stopAnalyser() {
-    this.audioContext?.close().then(() => {
-      this.audioContext = undefined;
-    });
-  }
-
-  // Get the frequencies and return values based on options
-  analysePitch() {
-    this.audioAnalyser.getFloatFrequencyData(this.frequencies!);
-
-    const frequency = this.calculateFrequency(this.frequencies!);
-
-    if (frequency) {
-      const { returnCents, returnNote, decimals, callback } = this.options;
-
-      const returnValue = {
-        frequency: frequency.toFixed(decimals),
-        note: "",
-        cents: "",
-      };
-
-      if (returnNote) {
-        const note = this.calculateNote(frequency);
-        returnValue.note = note;
-      }
-
-      if (returnNote && returnCents) {
-        if (this.lastFrequency) {
-          const cents = this.calculateCents(frequency, this.lastFrequency);
-          returnValue.cents = cents.toFixed(decimals);
-        }
-        this.lastFrequency = frequency;
-      }
-
-      // Execute the callback. (Intended for returning the output)
-      callback(returnValue);
-    }
-
-    // Tells the browser we wish to perform an animation. Call callback before re-paint
-    if (this.audioContext && this.audioContext.state === 'running') {
-      window.requestAnimationFrame(this.analysePitch);
-    }
-  }
-
-  initialize() {
-    // Start media stream
-    return this.mediaDevices.getUserMedia({ audio: true })
-      .then(stream => {
-        this.initiateStream(stream);
-        })
-      .catch(this.throwError);
-  }
+  // stopAnalyser() {
+  //   this.audioContext?.close().then(() => {
+  //     this.audioContext = undefined;
+  //   });
+  // }
 
   initiateStream(stream: MediaStream) {
     // Set the stream to audioStream
@@ -199,63 +174,32 @@ export class GeneralNoteDetectorService {
     this.audioSource.connect(this.audioAnalyser);
   }
 
-	// What to do with the audio payload
-	analyserCallback(payload: {frequency: string; note: string; cents: string;}) {
-		this.note = payload.note;
-		this.frequency = payload.frequency;
-		this.cents = payload.cents;
-	}
+    //   // What to do with the audio payload
+  //   analyserCallback(payload: { frequency: string; note: string; cents: string; }) {
+  //     this.note = payload.note;
+  //     this.frequency = payload.frequency;
+  //     this.cents = payload.cents;
+  //   }
+
+  // // Button to initialise and start the analyser
+  // startButton() {
+  //   // Required to init the analyser first
+  //   // It will create an AudioContext instance under 'analyser.audioContext'
+  //   // It returns no value
+  //   this.generalNoteService.initialize({callback : this.analyserCallback}).then(() => {
+  //     // Start the analyser after initialisation
+  //     this.generalNoteService.analysePitch();
+  //   });
+  // };
+
+  // controlPlay() {
+  //   this.generalNoteService.resumeAnalyser();
+  // };
+
+  // controlPause() {
+  //   this.generalNoteService.pauseAnalyser();
+  // };
 
 
-	// Button to initialise and start the analyser
-	startButton(){
-		// Required to init the analyser first
-		// It will create an AudioContext instance under 'analyser.audioContext'
-		// It returns no value
-		this.initAnalyser().then(() => {
-			// Start the analyser after initialisation
-			this.startAnalyser();
-		});
-    let startButton = document.querySelector('main > div:first-of-type button');
-		startButton!.classList.add('hidden');
-	};
-
-	controlPlay(){
-		if (this.audioContext?.state === 'suspended') {
-			this.resumeAnalyser();
-		}
-	};
-
-	controlPause() {
-		if (this.audioContext?.state === 'running') {
-			this.pauseAnalyser();
-		}
-	};
-
-
-  // -- Possible stream pitch analyzer
-
-//   if ("mediaDevices" in navigator) {
-//     navigator.mediaDevices
-//       .getUserMedia({ audio: true })
-//       .then(
-//           stream => {
-//             audioContext.createMediaStreamSource(stream).connect(analyser);
-
-//             const arrayUInt = new Uint8Array(analyser.frequencyBinCount);
-//             analyser.getByteTimeDomainData(arrayUInt);
-
-//             setInterval(() => {
-//               const array32 = new Float32Array(analyser.fftSize);
-//               analyser.getFloatTimeDomainData(array32);
-//               console.log(array32);
-
-//               const pitch = detectPitch(array32);
-
-//               console.log(pitch);
-//             }, 500);
-//           })
-//       .catch(() => {});
-// }
 
 }
--- src/app/views/note-detection/note-detection.component.ts
+++ src/app/views/note-detection/note-detection.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as Pitchfinder from "pitchfinder";
+import { PitchDetector } from 'pitchfinder/lib/detectors/types';
 
 
 @Component({
@@ -8,10 +10,96 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteDetectionComponent implements OnInit {
 
+  started = false;
+  note: string | null = "";
+  frequency: number | null = null;
+  cents: number | null = null;
+  lastFrequency: number | null = null;
+  algorithms = ['YIN', 'AMDF', 'ACF2PLUS', 'DynamicWavelet', 'Macleod'];
+
+  private detectPitch!: PitchDetector;
+  private notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+  private A4 = 440;
+  private C0 = Math.round(this.A4 * Math.pow(2, -4.75)); // 16
+
+  // Predefine variables
+  private audioAnalyser: AnalyserNode;
+  private audioContext: AudioContext;
+
   constructor() {
+    this.audioContext = new window.AudioContext();
+    this.audioAnalyser = this.audioContext!.createAnalyser();
   }
 
   ngOnInit(): void {
+    this.detectPitch = Pitchfinder.YIN();
+    // this.detectPitch = Pitchfinder.AMDF();
+    // this.detectPitch = Pitchfinder.ACF2PLUS();
+    // this.detectPitch = Pitchfinder.DynamicWavelet();
+    // this.detectPitch = Pitchfinder.Macleod();
+  }
+
+  stop() {
+    this.audioContext.suspend();
+  }
+
+  calculateNote(frequency: number | null) {
+    if (frequency === null) {
+      return ""
+    }
+    const semiTone = this.calculateSemiTone(frequency);
+    const octave = this.calculateOctave(semiTone);
+    const notePosition = Math.floor(semiTone % 12);
+    const note = this.notes[notePosition] + String(octave);
+    return note;
+  };
+
+  // Calculate amount of steps away from C0
+  calculateSemiTone(frequency: number) {
+    const semiTonesAway = 12 * Math.log2(frequency / this.C0);
+    return semiTonesAway;
+  };
+
+  // Uses C0 as base
+  calculateOctave(semiTonesAway: number) {
+    const octave = Math.floor(semiTonesAway / 12);
+    return octave;
+  };
+
+  calculateCents(currentFrequency: number | null, lastFrequency: number | null) {
+    return (lastFrequency && currentFrequency) ? 1200 * Math.log2(lastFrequency / currentFrequency) : 0;
+  };
+
+  setPitchDetector(){
+
+  }
+
+
+  test() {
+    this.started = true;
+
+    if ("mediaDevices" in navigator) {
+      navigator.mediaDevices
+        .getUserMedia({ audio: true })
+        .then(
+          stream => {
+            this.audioContext!.createMediaStreamSource(stream).connect(this.audioAnalyser);
+
+            const arrayUInt = new Uint8Array(this.audioAnalyser.frequencyBinCount);
+            this.audioAnalyser.getByteTimeDomainData(arrayUInt);
+
+            setInterval(() => {
+              const array32 = new Float32Array(this.audioAnalyser.fftSize);
+              this.audioAnalyser.getFloatTimeDomainData(array32);
+
+              this.frequency = this.detectPitch(array32);
+              this.note = this.calculateNote(this.frequency);
+              this.cents = this.calculateCents(this.frequency, this.lastFrequency);
+
+            }, 500);
+          })
+        .catch(() => { });
+    }
   }
 
 }

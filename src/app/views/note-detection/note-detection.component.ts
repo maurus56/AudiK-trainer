eb7daa8c78ec1c90a707e8434dcdc2395aad07e3
maurus56 <maurus56@gmail.com>
2022-07-11 15:32:37 +0200

    added simultaneous algorithm detection

--- src/app/views/note-detection/note-detection.component.ts
+++ src/app/views/note-detection/note-detection.component.ts
@@ -1,7 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import * as Pitchfinder from "pitchfinder";
-import { PitchDetector } from 'pitchfinder/lib/detectors/types';
+import { PitchDetector, ProbabalisticPitchDetector, ProbabilityPitch } from 'pitchfinder/lib/detectors/types';
 
+interface algorithm {
+  name: string,
+  algo: PitchDetector | ProbabalisticPitchDetector,
+  active: boolean,
+  data: {
+    note?: string | ProbabilityPitch | null,
+    frequency?: number | null,
+    cents?: number | null,
+    lastFrequency?: number | null
+  }
+};
 
 @Component({
   selector: 'app-note-detection',
@@ -11,13 +22,27 @@ import { PitchDetector } from 'pitchfinder/lib/detectors/types';
 export class NoteDetectionComponent implements OnInit {
 
   started = false;
-  note: string | null = "";
-  frequency: number | null = null;
-  cents: number | null = null;
-  lastFrequency: number | null = null;
-  algorithms = ['YIN', 'AMDF', 'ACF2PLUS', 'DynamicWavelet', 'Macleod'];
 
-  private detectPitch!: PitchDetector;
+  settings = {
+    YIN: {
+      threshold: 0,
+      probabilityThreshold: 0
+    },
+    AMDF: {
+      minFrequency: 0,
+      maxFrequency: 0,
+      sensitivity: 0,
+      ratio: 0
+    }
+  };
+
+  private algorithms :algorithm[]  = [
+    { name: 'YIN', algo: Pitchfinder.YIN(), active: false, data: {} },
+    { name: 'AMDF', algo: Pitchfinder.AMDF(), active: false, data: {} },
+    { name: 'ACF2PLUS', algo: Pitchfinder.ACF2PLUS(), active: false, data: {} },
+    { name: 'DynamicWavelet', algo: Pitchfinder.DynamicWavelet(), active: false, data: {} },
+    { name: 'Macleod', algo: Pitchfinder.Macleod(), active: false, data: {} }
+  ];
   private notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
   private A4 = 440;
   private C0 = Math.round(this.A4 * Math.pow(2, -4.75)); // 16
@@ -32,16 +57,79 @@ export class NoteDetectionComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.detectPitch = Pitchfinder.YIN();
-    // this.detectPitch = Pitchfinder.AMDF();
-    // this.detectPitch = Pitchfinder.ACF2PLUS();
-    // this.detectPitch = Pitchfinder.DynamicWavelet();
-    // this.detectPitch = Pitchfinder.Macleod();
+    console.log(this.audioAnalyser.fftSize);
   }
 
   stop() {
     this.audioContext.suspend();
   }
+
+  togglePitchDetector(algo: string) {
+    if (!this.algorithms.some(algos => algos.name === algo)) { return };
+
+    this.algorithms = this.algorithms.map(obj => {
+      if (obj.name === algo) {
+        obj.active = !obj.active;
+      };
+      return obj;
+    });
+  }
+
+  getAlgorithms() {
+    return this.algorithms.map(obj => { return { name: obj.name, active: obj.active, data: obj.data } });
+  }
+
+  getData() {
+    return this.algorithms.filter(obj => obj.active).map(obj => { return { name: obj.name, data: obj.data } });
+  }
+
+
+  test() {
+    this.started = true;
+
+    if ("mediaDevices" in navigator) {
+      navigator.mediaDevices
+        .getUserMedia({ audio: true })
+        .then(
+          stream => {
+            this.audioContext!.createMediaStreamSource(stream).connect(this.audioAnalyser);
+
+            const arrayUInt = new Uint8Array(this.audioAnalyser.frequencyBinCount);
+            this.audioAnalyser.getByteTimeDomainData(arrayUInt);
+
+            setInterval(() => {
+              const array32 = new Float32Array(this.audioAnalyser.fftSize);
+              this.audioAnalyser.getFloatTimeDomainData(array32);
+
+              this.algorithms.forEach(obj => {
+                if (obj.active) {
+
+                  let frequency = obj.algo(array32);
+                  let probability: number;
+                  let lastFrequency = obj.data.frequency ?? 0;
+
+                  if (frequency && typeof (frequency) === 'object') {
+                    probability = frequency.probability;
+                    frequency = frequency.freq;
+                  };
+
+                  const values = {
+                    note: this.calculateNote(frequency),
+                    frequency: frequency,
+                    cents: this.calculateCents(frequency, lastFrequency),
+                    lastFrequency: lastFrequency,
+                  };
+
+                  obj.data = values;
+                }
+              });
+
+            }, 500);
+          })
+        .catch(() => { });
+    }
+  }
+
 
   calculateNote(frequency: number | null) {
     if (frequency === null) {
@@ -69,37 +157,4 @@ export class NoteDetectionComponent implements OnInit {
   calculateCents(currentFrequency: number | null, lastFrequency: number | null) {
     return (lastFrequency && currentFrequency) ? 1200 * Math.log2(lastFrequency / currentFrequency) : 0;
   };
-
-  setPitchDetector(){
-
-  }
-
-
-  test() {
-    this.started = true;
-
-    if ("mediaDevices" in navigator) {
-      navigator.mediaDevices
-        .getUserMedia({ audio: true })
-        .then(
-          stream => {
-            this.audioContext!.createMediaStreamSource(stream).connect(this.audioAnalyser);
-
-            const arrayUInt = new Uint8Array(this.audioAnalyser.frequencyBinCount);
-            this.audioAnalyser.getByteTimeDomainData(arrayUInt);
-
-            setInterval(() => {
-              const array32 = new Float32Array(this.audioAnalyser.fftSize);
-              this.audioAnalyser.getFloatTimeDomainData(array32);
-
-              this.frequency = this.detectPitch(array32);
-              this.note = this.calculateNote(this.frequency);
-              this.cents = this.calculateCents(this.frequency, this.lastFrequency);
-
-            }, 500);
-          })
-        .catch(() => { });
-    }
-  }
-
 }
